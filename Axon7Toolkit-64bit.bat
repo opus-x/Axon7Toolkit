58d05fe8ad172424c3f4148c79a6eee08185bc29
bennykor <Benkores@gmail.com>
2017-02-20 16:03:00 -0500

    Update Axon7Toolkit-64bit.bat

--- Axon7Toolkit-64bit.bat
+++ Axon7Toolkit-64bit.bat
@@ -1,3 +1,7 @@
 @echo off
 color 1f
+echo *************************************************
+echo *  Axon7Tool v1.0
+echo *  by benkores
+echo *************************************************
 pause
