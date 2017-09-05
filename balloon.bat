b722553325bfe72413d56892023a59c5e7ac9194
bennykor <Benkores@gmail.com>
2017-09-05 11:28:13 -0400

    Delete balloon.bat

--- balloon.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set type=%~1
-set duration=%~2
-set message=%~3
-set toolpath=C:\Axon7Development\Axon7Toolkit
-
-%toolpath%\notifu /t %type% /d %duration% /p "Axon7Toolkit" /m "%message%" /i %toolpath%\icons\toolkit.ico
